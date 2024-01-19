6e34bff5349f6813347a63ab44354349b58dc952
sowbaranika <sati.sow@gmail.com>
2024-01-19 15:32:43 +0530

    app.js updated

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -2,11 +2,9 @@ import axios from "axios";
 
 const instance = axios.create({
   // .. congigure axios baseURL
-<<<<<<< HEAD
+
   baseURL: "https://zen-dashboard-sowbi.onrender.com"             //https://zen-student-dashboard.onrender.com/ http://localhost:8000
-=======
-  baseURL: "https://zen-student-dashboard.onrender.com"
->>>>>>> 7be96f202086f43148156f1f1c93e36d88c3270f
+
 });
 
 
