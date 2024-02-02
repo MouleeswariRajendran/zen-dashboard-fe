c2f90985cf2c3a476aeb9450f04e226489852be7
unknown <rmouleeswari@gmail.com>
2024-02-02 16:11:20 +0530

    update

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   // .. congigure axios baseURL
 
-  baseURL: "https://zen-dashboard-sowbi.onrender.com"             //https://zen-student-dashboard.onrender.com/ http://localhost:8000
+  baseURL: "https://zen-dashboard-be.onrender.com"             //https://zen-student-dashboard.onrender.com/ http://localhost:8000
 
 });
 
