f7bff007e3acec1cdbe1a2ec0240298c5109ba58
Mouli <145764370+MouleeswariRajendran@users.noreply.github.com>
2024-05-06 09:39:55 +0530

    Update Api.jsx

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -3,7 +3,15 @@ import axios from "axios";
 const instance = axios.create({
   // .. congigure axios baseURL
 
-  baseURL: "https://zen-dashboard-be.onrender.com"             //https://zen-student-dashboard.onrender.com/ http://localhost:8000
+  //baseURL: "https://zen-dashboard-be.onrender.com"
+  
+  baseURL:"https://zen-dashboard-be.onrender.com/"
+  
+  
+  
+  
+  
+  //https://zen-student-dashboard.onrender.com/ http://localhost:8000
 
 });
 
